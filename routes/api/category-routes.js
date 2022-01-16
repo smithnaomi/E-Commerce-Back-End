96c7471cc073c40ce7cb4e4f87d03764e956c89a
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-01-16 18:22:03 -0500

    Added deleted Category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -64,6 +64,25 @@ router.put("/:id", async (req, res) => {
 });
 
 // delete a category by its `id` value
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with this id!" });
+      return;
+    }
+
+    res
+      .status(200)
+      .json({ message: "Deleted " + req.params.id + " successfully!" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;

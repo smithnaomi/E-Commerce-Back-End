2c5ee4e7e5370e34d2afbdc6a17a014c3f98af78
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-01-16 23:54:00 -0500

    routers

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,19 +5,82 @@ const { Tag, Product, ProductTag } = require("../../models");
 
 // find all tags
 // be sure to include its associated Product data
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({ include: [{ model: Product }] });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // find a single tag by its `id`
 // be sure to include its associated Product data
-router.get("/:id", async (req, res) => {});
+router.get("/:id", async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with this id!" });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // create a new tag
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 // update a tag's name by its `id` value
-router.put("/:id", async (req, res) => {});
+router.put("/:id", async (req, res) => {
+  Tag.update(
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedTag) => {
+      res.json(updatedTag);
+    })
+    .catch((err) => res.json(err));
+});
 
 // delete on tag by its `id` value
-router.delete("/:id", async (req, res) => {});
+router.delete("/:id", async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with this id!" });
+      return;
+    }
+
+    res
+      .status(200)
+      .json({ message: "Deleted " + req.params.id + " successfully!" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;

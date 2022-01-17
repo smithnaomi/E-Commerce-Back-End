f89cab87acca3539b5946de48eb481256bd7b722
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-01-16 23:16:51 -0500

    cleaned code

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,23 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
-});
+// find all tags
+// be sure to include its associated Product data
+router.get("/", (req, res) => {});
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-});
+// find a single tag by its `id`
+// be sure to include its associated Product data
+router.get("/:id", (req, res) => {});
 
-router.post('/', (req, res) => {
-  // create a new tag
-});
+// create a new tag
+router.post("/", (req, res) => {});
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
+// update a tag's name by its `id` value
+router.put("/:id", (req, res) => {});
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
-});
+// delete on tag by its `id` value
+router.delete("/:id", (req, res) => {});
 
 module.exports = router;
